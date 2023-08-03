2cf98573074dcdc0dc903cbd695ebcf8df55d1c6
fuuron <koki1016jp@gmail.com>
2023-08-03 14:19:48 +0900

    Laravel 8入門 CRUD処理編

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -22,5 +23,45 @@ class PostController extends Controller
     {
         return view('posts.show')
             ->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(PostRequest $request)
+    {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()
+            ->route('posts.index');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit')
+            ->with(['post' => $post]);
+    }
+
+    public function update(PostRequest $request, Post $post)
+    {
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()
+            ->route('posts.show', $post);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()
+        ->route('posts.index');
     }
 }
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,43 @@
+<x-layout>
+    <x-slot name="title">
+        Edit Post - My BBS
+    </x-slot>
+
+    <div class="container">
+        <div class="back-link">
+            &laquo; <a href="{{ route('posts.show', $post) }}">Back</a>
+        </div>
+
+        <h1>Edit Post</h1>
+    </div>
+
+    <form method="post" action="{{ route('posts.update', $post) }}">
+        @method('PATCH')
+        @csrf
+
+        <div class="form-group">
+            <label>
+                Title
+                <input type="text" name="title" value="{{ old('title', $post->title) }}">
+            </label>
+            @error('title')
+                <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label>
+                Body
+                <textarea name="body">{{ old('body', $post->body) }}</textarea>
+            </label>
+            @error('body')
+                <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="form-button">
+            <button>Update</button>
+        </div>
+
+    </form>
+</x-layout>
